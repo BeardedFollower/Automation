2b458179c5ca5c2f7884f5ed77d4d1e3ec5fd3c5
BeardedFollower <89873294+BeardedFollower@users.noreply.github.com>
2025-02-12 15:27:35 -0500

    Update Activate-AD-PIM-ROLES.ps1
    add some logic to check to see if modules installed.

--- Activate-AD-PIM-ROLES.ps1
+++ Activate-AD-PIM-ROLES.ps1
@@ -1,30 +1,32 @@
-﻿<#
-This PowerShell module contains a collection of tools for Microsoft 365 security tasks
-such as Microsoft Graph functions, Azure AD management, Conditional Access, zero trust strategies, attack and defense scenarios, etc.
-#>
-#Install-Module -Name DCToolbox -Force -Scope AllUsers
+## ----------------------------------------------------------------------- ##
+## Script Created to automatically activate Microsoft Entra Privileged Identity Management 
+## Created by BeardedFollower
+## Last Updated 02/13/2025
+## ----------------------------------------------------------------------- ##
+## To Use: copy ps script to machine, run script, log into account
 
-<#
-PowerShell module for MSAL. MSAL.NET (Microsoft.Identity.Client) is an authentication library which enables you to acquire tokens from Azure AD,
-to access protected Web APIs (Microsoft APIs or applications registered with Azure Active Directory).
-#>
-#Install-Package msal.ps -Force -Scope AllUsers
+## Variables ##
+## Define Roles to Activate Here ##
+$Roles = 'Authentication Administrator' , 'Groups Administrator' , 'Printer Administrator' , 'Reports Reader' , 'Security Reader' , 'User Administrator', 'Service Support Administrator' , 'User Administrator'
+$ReasonForActivation = Read-Host
 
-<#
-NOTE: If you have AzureAD module installed, please remove first and then install the AzureADPreview
-#>
-#Install-Module -Name AzureADPreview -Force -Scope AllUsers
+#This PowerShell module contains a collection of tools for Microsoft 365 security tasks, Microsoft Graph functions, Azure AD management, Conditional Access, zero trust strategies, attack and defense scenarios, etc.#
+if(-not (Get-Module DCToolbox -ListAvailable)){
+    Install-Module DCToolbox -Scope AllUsers -Force
+    }
 
-<#
-The RemoteSigned policy allows us to run scripts that we have created locally, but scripts that we download must be digitally signed by a trusted publisher. The Unrestricted policy carries no restrictions at all, and allows us to run unsigned scripts from any source.#
-#>
-#Set-ExecutionPolicy RemoteSigned
+#PowerShell module for MSAL. MSAL.NET (Microsoft.Identity.Client) is an authentication library which enables you to acquire tokens from Azure AD, to access protected Web APIs (Microsoft APIs or applications registered with Azure Active Directory).#  
+if(-not (Get-Package msal.ps)){
+    Install-Package msal.ps -Force -Scope AllUsers
+    } 
 
-<#
-After "RolesToActivate" you will enter the name of the role you want active, the format can been seen below.
-There doesn't seem to be a limited on how many you can add. 
-#>
-Enable-DCAzureADPIMRole -RolesToActivate 'User Administrator', 'Authentication Administrator' , 'Service Support Administrator' , 'Printer Administrator' , 'Groups Administrator' , 'Security Reader' -UseMaximumTimeAllowed -Reason 'Work Day'
+#This checkes to see if AzureAD Preview is installed and if not install it.
+if(-not (Get-Module AzureADPreview -ListAvailable)){
+    Install-Module AzureADPreview -Scope AllUsers -Force
+    } 
 
+# geThe RemoteSigned policy allows us to run scripts that we have created locally, but scripts that we download must be digitally signed by a trusted publisher. The Unrestricted policy carries no restrictions at all, and allows us to run unsigned scripts from any source.
+# Set-ExecutionPolicy RemoteSigned
 
-#After you have completed the installs, and ran Set-ExecutionPolicy RemoteSigned you no longer need to run these. You can copy the "Enable-DCAzureADPIMRole -RolesToActivate xxxx" over into it's own script and save to desktop and just run it normally. 
+#After "RolesToActivate" you will enter the name of the role you want active, the format can been seen from above. There doesn't seem to be a limited on how many you can add. 
+Enable-DCAzureADPIMRole -RolesToActivate $Roles -UseMaximumTimeAllowed -Reason $ReasonForActivation
